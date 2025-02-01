d89dfdde8cb5b2e425460c9bc6dcde44ac039f6a
artimatic <dvxchan@gmail.com>
2025-01-31 20:44:03 -0500

    Removed unused

--- src/app/autopilot/autopilot.component.ts
+++ src/app/autopilot/autopilot.component.ts
@@ -1181,7 +1181,7 @@ export class AutopilotComponent implements OnInit, OnDestroy {
         await this.buyWinners();
         break;
       case Strategy.PerfectPair:
-        await this.autopilotService.addPerfectPair(this.currentHoldings);
+        await this.autopilotService.addPerfectPair();
         break;
       case Strategy.BuyML:
         await this.autopilotService.getAnyBuy();
