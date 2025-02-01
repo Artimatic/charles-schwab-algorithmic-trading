dd05e15c9c948d5b74dff7203eae08c8a0f588f8
artimatic <dvxchan@gmail.com>
2025-01-31 20:42:56 -0500

    Update autopilot.component.ts

--- src/app/autopilot/autopilot.component.ts
+++ src/app/autopilot/autopilot.component.ts
@@ -757,7 +757,7 @@ export class AutopilotComponent implements OnInit, OnDestroy {
           } else if (!holding.secondaryLegs) {
             //this.optionsOrderBuilderService.hedgeTrade(holding.name, this.currentHoldings);
           }
-        } else if ((backtestResults && (backtestResults.net < 0 || backtestResults.recommendation === 'STRONGSELL' || backtestResults.recommendation === 'SELL' || holding.name === 'TQQQ'))) {
+        } else if ((backtestResults && (backtestResults.recommendation === 'STRONGSELL' || backtestResults.recommendation === 'SELL' || holding.name === 'TQQQ'))) {
           console.log('Backtest indicates sell', backtestResults);
           await this.cartService.portfolioSell(holding, 'Backtest indicates sell');
         } else if (backtestResults && backtestResults.ml !== null && backtestResults.ml > 0.7 && (backtestResults.recommendation === 'STRONGBUY' || backtestResults.recommendation === 'BUY')) {
