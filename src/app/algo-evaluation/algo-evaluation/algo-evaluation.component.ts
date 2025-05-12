dd9069939a74666f64baebcb524fef8e355e9873
David Chan <dvxchan@gmail.com>
2025-05-12 17:39:16 -0400

    Update algo-evaluation.component.ts

--- src/app/algo-evaluation/algo-evaluation/algo-evaluation.component.ts
+++ src/app/algo-evaluation/algo-evaluation/algo-evaluation.component.ts
@@ -41,17 +41,9 @@ export class AlgoEvaluationComponent implements OnInit {
     this.recommendations = this.stockList.filter(stock => {
       if ((stock?.ml > 0.5) && (stock.recommendation.toLowerCase() === 'buy' || stock.recommendation.toLowerCase() === 'strongbuy')) {
         stock.recommendation = 'Strong buy';
-        if (stock.impliedMovement < 0.09) {
-          this.optionsOrderBuilderService.addCallToCurrentTrades(stock.stock);
-        } else {
-          this.strategyBuilderService.addBullishStock(stock.stock);
-        }
         return true;
       } else if ((stock?.sellMl > 0.5) && (stock.recommendation.toLowerCase() === 'sell' || stock.recommendation.toLowerCase() === 'strongsell')) {
         stock.recommendation = 'Strong sell';
-        if (stock.impliedMovement < 0.09) {
-          this.optionsOrderBuilderService.addPutToCurrentTrades(stock.stock);
-        }
         return true;
       }
       return false;
