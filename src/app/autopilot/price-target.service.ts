2444b228040a592e11c2aff702d55fe4697dcda7
David Chan <dvxchan@gmail.com>
2025-08-02 23:55:47 -0400

    Update price-target.service.ts

--- src/app/autopilot/price-target.service.ts
+++ src/app/autopilot/price-target.service.ts
@@ -37,7 +37,7 @@ export class PriceTargetService {
     let portfolioVolatility = await this.portfolioWeightsService.getPortfolioVolatility(holdings);
     this.portfolioVolatility = round(portfolioVolatility, 2);
     const tenYrYield = await this.globalSettingsService.get10YearYield();
-    const target = ((tenYrYield + 1.618034) * 0.01 * this.portfolioVolatility) + 0.018;
+    const target = ((tenYrYield + 1.618034) * (this.portfolioVolatility + 0.01)) + 0.018;
     this.targetDiff = round((!target) ? this.targetDiff : target, 4);
     this.reportingService.addAuditLog(null, `Target set to ${this.targetDiff}`);
     this.reportingService.addAuditLog(null, `Current portfolio volatility: ${this.portfolioVolatility}`);
